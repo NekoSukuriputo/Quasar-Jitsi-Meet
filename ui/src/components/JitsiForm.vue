781ef7468206ff33dac8ef7b2f6f45d2b3075204
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2022-10-14 08:41:52 +0700

    update using tab panels

--- ui/src/components/JitsiForm.vue
+++ ui/src/components/JitsiForm.vue
@@ -1,64 +1,113 @@
 <script setup>
-import { QForm, QBtn, QInput, QCard, QCardActions, QCardSection, QSeparator } from 'quasar'
-import { ref } from 'vue'
+import {
+  QForm,
+  QBtn,
+  QInput,
+  QCard,
+  QCardActions,
+  QCardSection,
+  QSeparator,
+  QTab,
+  QTabPanel,
+} from "quasar";
+import { ref } from "vue";
 
-let emit = defineEmits(['submit'])
+let emit = defineEmits(["submit"]);
 
-let roomName = ref('')
-let email = ref('')
-let displayName = ref('')
-let showAdd = ref(1)
+let roomName = ref("");
+let email = ref("");
+let displayName = ref("");
+let showAdd = ref(1);
+
+let tab = ref("make_room");
 
 const showForm = (value) => {
-  roomName.value = ''
-  email.value = ''
-  displayName.value = ''
-  showAdd.value = value
-}
+  roomName.value = "";
+  email.value = "";
+  displayName.value = "";
+  showAdd.value = value;
+};
 
 const onSubmit = () => {
-  emit('submit', {
+  emit("submit", {
     displayName: displayName.value,
-    email:email.value,
+    email: email.value,
     roomName: roomName.value,
-    isAddRoom: showAdd.value === 1
-  })
-}
+    isAddRoom: tab.value === "make_room",
+  });
+};
 </script>
 <template>
   <div>
-    <div class="q-pa-md row items-start q-gutter-md">
+    <div>
       <q-card>
-        <q-card-section>
-          <div class="q-mt-md" v-if="showAdd === 1">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="make_room" label="Make Room" />
+          <q-tab name="join_room" label="Join Room" />
+        </q-tabs>
+        <q-separator />
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="make_room">
             <q-form @submit="onSubmit" class="q-gutter-md">
-              <q-input outlined v-model="roomName" label="Room Name" dense
-                :rules="[val => val && val.length > 0 || 'Please type something']" />
-              <q-input outlined v-model="email" label="Email" dense
-                :rules="[val => val && val.length > 0 || 'Please type something']" />
-              <q-input outlined v-model="displayName" label="Display Name" dense
-                :rules="[val => val && val.length > 0 || 'Please type something']" />
+              <q-input
+                outlined
+                v-model="roomName"
+                label="Room Name"
+                dense
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              />
+              <q-input
+                outlined
+                v-model="email"
+                label="Email"
+                dense
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              />
+              <q-input
+                outlined
+                v-model="displayName"
+                label="Display Name"
+                dense
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              />
               <div>
                 <q-btn label="Submit" type="submit" color="primary" />
               </div>
             </q-form>
-          </div>
-          <div class="q-mt-md" v-if="showAdd === 2">
-            <q-form @submit="onSubmit" class="q-gutter-md">
-              <q-input outlined v-model="roomName" label="Room Name" dense
-                :rules="[val => val && val.length > 0 || 'Please type something']" />
-              <div>
-                <q-btn label="Submit" type="submit" color="primary" />
-              </div>
-            </q-form>
-          </div>
-        </q-card-section>
-        <q-separator dark />
+          </q-tab-panel>
 
-        <q-card-actions>
-          <q-btn flat label="Make Room" icon="add" @click="showForm(1)" />
-          <q-btn flat label="Join Room" icon="people" @click="showForm(2)" />
-        </q-card-actions>
+          <q-tab-panel name="join_room">
+            <div class="text-h6">Join Room</div>
+            <q-form @submit="onSubmit" class="q-gutter-md">
+              <q-input
+                outlined
+                v-model="roomName"
+                label="Room Name"
+                dense
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Please type something',
+                ]"
+              />
+              <div>
+                <q-btn label="Submit" type="submit" color="primary" />
+              </div>
+            </q-form>
+          </q-tab-panel>
+        </q-tab-panels>
       </q-card>
     </div>
   </div>
